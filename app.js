79a8a3c74758fbf545e3e3468399a24d38eb8c48
xuanngo10 <xuan.ngo10@gmail.com>
2021-05-27 00:00:04 -0700

    progress

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const bodyParser = require("body-parser");
+const passport = require("passport");
 const app = express();
 
 const db = require("./config/keys").mongoURI;
@@ -9,7 +13,18 @@ mongoose
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch((err) => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello Earth"));
+//   Body parser is deprecated
+// app.use(bodyParser.urlencoded({extended: false}));
+// app.use(bodyParser.json());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+
+app.use(passport.initialize());
+require("./config/passport")(passport);
 
 const port = process.env.PORT || 5000;
 
---
+++ frontend/src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Root from "./components/root";
+import configureStore from "./store/store";
+import jwt_decode from "jwt-decode";
+import { setAuthToken } from "./util/session_api_util";
+import { logout } from "./actions/sessions_actions";
+
+document.addEventListener("DOMContentLoaded", () => {
+  let store;
+
+  // If a returning user has a session token stored in localStorage
+  if (localStorage.jwtToken) {
+    // Set the token as common header for all axios requests
+    setAuthToken(localStorage.jwtToken);
+    // Decode the token to obtain the user's information
+  }
+});
